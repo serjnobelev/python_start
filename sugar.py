eb1d65a1dbd059d38131d57322640fba0594a030
serj <s.nobelev@gmail.com>
2019-05-10 12:48:01 +0300

    sugar

---
+++ sugar.py
@@ -0,0 +1,15 @@
+# списковые выражения
+
+# s = []
+# for i in 'string':
+#     s.append(i)
+s = [i for i in 'string']
+print(s)
+
+# n = []
+# for i in range(1,15):
+#     if i % 2 == 0:
+#         n.append(i)
+n = [i for i in range(1,15) if i % 2 == 0]
+print(n)
+
