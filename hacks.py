3782b2cd169e5832d6edd682dabb7c4f3ec09710
serj <s.nobelev@gmail.com>
2019-05-16 23:00:47 +0300

    some hacks update

--- hacks.py
+++ hacks.py
@@ -4,14 +4,18 @@
 # s, k = (1, 2)
 # print(k)
 
+
 # s, k = [1, 2]
 # print(s, k)
+
 
 # s, *k = [1, 2, 3, 4]
 # print(s, k)
 
+
 # first, *str = 'my str'
 # print(str)
+
 
 # def my_fnc():
 #     x1 = 1
@@ -26,23 +30,31 @@ def accepted_args(*args):
     print(args)
     print(sum(args))
 
+
 accepted_args(1, 2, 3)
+
 
 values = [1, 2 ,3]
 # accepted_args(values)
 
+
 # Convert list to tuple
 accepted_args(*values)
 
+
 # kwargs is the same
+
 
 def fnc_kwargs(**kwargs):
     print(kwargs)
 
+
 my_dict = {'day': 'wed', 'month': 'jan'}
+
 
 # Convert dict to kwargs
 fnc_kwargs(**my_dict)
+
 
 # array creating
 arr = [f for f in '123']
